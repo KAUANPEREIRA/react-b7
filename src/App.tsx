e6f7df9dd39dd45c3545a5b8ebcc0bfbbcc6c4d7
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2022-06-24 15:50:04 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,35 @@ function App() {
 
   const [name, setName] = useState('')
   const [lastName,setLastName]= useState('')
-  const [age,setAge] = useState(0)
+  const [age,setAge] = useState('')
 
   const handleName =(e:React.ChangeEvent<HTMLInputElement>)=>{
     setName(e.target.value)
-    setLastName(e.target.value)
-    setAge(e.target.value)
-    
+
   }
+
+  const handleLastName =(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setLastName(e.target.value)
+  }
+
+  const handleAge =(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setAge(e.target.value)
+  }
+  
+
 
   
 
   return (
     <div className="App">
       nome:
-      <input type="text" value={name} onChange={handleName} placeholder="digite seu nome"/>
+      <input type="text" value={name } onChange={handleName} placeholder="digite seu nome"/>
+      <input type="text" value={ lastName } onChange={handleLastName} placeholder="digite seu Sobrenome"/>
+      <input type="text" value={ age } onChange={handleAge} placeholder="digite sua idade"/>
       <hr></hr>
-      Seu nome é: {name}
+      Seu nome é: {name } 
+      { lastName } 
+      e sua idade é { age} anos
       
     </div>
   );
